1c3a4f2a991b02e6226301834d316f8b08513b48
DESKTOP-GOA90O0\Theo Basallaje <theobasallaje@gmail.com>
2019-01-08 19:24:59 -0600

    added timer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+//arrays
 var questions = [
     {
         question: "What does the online acronym SMH stand for?",
@@ -15,23 +16,50 @@ var questions = [
         answer: "High-Definition Multimedia Interface"
     }
 ];
-
+//objects
 var results = {
 
 }
 
+//global variables
+var questionsElement = $("#questions");
+var submit = $("#submit");
+var timerElement = $("#timer");
+var timer = 60
+var timerInterval;
+
 var correct = 0;
+var wrong = 0;
 
+//functions
 function startGame(){
-
+    correct = 0;
+    wrong=0;
+    timer=60;
+    timerInterval = setInterval(function(){ 
+        if(timer > 1){ 
+            timer-- 
+            timerElement.html("Timer: " + timer);
+        } else{ 
+            alert("time out")
+            clearInterval(timerInterval)
+        } 
+    }
+    
+    , 1000)
+    timerElement.html("Timer: " + timer);
+    questionsElement.empty();
+    displayQuestions();
 }
+
+startGame()
 
 
 function displayQuestions(){
     for (var i=0; i<questions.length; i++){
-        $("#questions").append(`<div class="card">${questions[i].question}</div>`);
+        questionsElement.append(`<div class="card">${questions[i].question}</div>`);
         for (var j=0; j< questions[i].options.length; j++){
-            $("#questions").append(`<input number=${i+1} data-value="${questions[i].options[j]}" name=${questions[i].question} type="radio" class="options">${questions[i].options[j]}`);
+            questionsElement.append(`<input number=${i+1} data-value="${questions[i].options[j]}" name=${questions[i].question} type="radio" class="options">${questions[i].options[j]}`);
         }
     }
 }
@@ -45,14 +73,21 @@ $(document).on('click', '.options', function(){
     console.log(results);
 })
 
-$("#submit").on('click', function(){
+submit.on('click', function(){
     for (var i=0; i<questions.length; i++){
         if (questions[i].answer === results[i+1]){
             correct++;
         }
+        else {
+            wrong++;
+        }
     }
+    clearInterval(timerInterval);
     console.log(correct);
+    console.log(wrong);
+    $('#questions').html(`<div> you had this many correct :  ${correct}</div>
+                        <div> you had this many wrong : ${wrong}</div>`);
+    setTimeout(startGame, 2000)
 })
 
-displayQuestions();
 
